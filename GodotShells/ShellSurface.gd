986232e269e44021842102d63e255f77ea4ff05f
Anders Steen <steenishpost@gmail.com>
2023-11-11 01:51:52 +0100

    add fur length and fur color textures

--- GodotShells/ShellSurface.gd
+++ GodotShells/ShellSurface.gd
@@ -1,19 +1,13 @@
 extends MeshInstance3D
 
 # TODO
-# Length mapping ends up with cut off strands
-# Should be darker further down
-# Should use length map texture
-# Should use fur color texture
 # Physics
-# XR<
+# XR
 
 @export var density : int = 100
-@export var color : Color
+@export var noiseMax : float
+@export var maxHeight : float
 @export var shellCount : int = 16
-@export var totalHeight : float = 0.2
-@export var maxWidth : float = 0.7
-@export var indexWidthMapping : Curve
 
 #var shellMaterials : Array[Material]
 
@@ -24,7 +18,8 @@ func construct_shells():
 #	shellMaterials = []
 	var shellMaterial = self.get_active_material(0)
 	shellMaterial.set_shader_parameter("_Density", density)
-	shellMaterial.set_shader_parameter("_Color", color)
+	shellMaterial.set_shader_parameter("_NoiseMax", noiseMax)
+	shellMaterial.set_shader_parameter("_MaxHeight", maxHeight)
 	for shellIndex in shellCount:
 		var shell = MeshInstance3D.new()
 		shell.name = "Shell %d" % shellIndex
@@ -34,14 +29,8 @@ func construct_shells():
 #		var shellMaterial = shell.get_active_material(0)
 #		shellMaterials.append(shellMaterial)
 		
-		var normalizedIndex = float(shellIndex) / (shellCount - 1)
-		var threshold = lerp(0.0, 1.0, normalizedIndex)
-		var width = maxWidth * indexWidthMapping.sample(normalizedIndex);
-		var height = lerp(0.0, totalHeight, normalizedIndex)
-		
-		shell.set_instance_shader_parameter("_Threshold", threshold)
-		shell.set_instance_shader_parameter("_Width", width)
-		shell.set_instance_shader_parameter("_Height", height)
+		var normalizedHeight = float(shellIndex) / (shellCount - 1)
+		shell.set_instance_shader_parameter("_NormalizedHeight", normalizedHeight)
 
 
 func _process(delta):
